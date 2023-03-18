1635042e1fdde05100b407022341235e05e0405a
jeeva9949 <goddumarrijeevananda@gmail.com>
2023-03-18 17:05:57 +0530

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+// import firebase from "./firebase";
+// const firebaseConfig = {
+//   apiKey: "AIzaSyD1Sf_kwHC7g8lOqWEZEpamVAmujcRZM6Y",
+//   authDomain: "linkedin-clone-d0c23.firebaseapp.com",
+//   projectId: "linkedin-clone-d0c23",
+//   storageBucket: "linkedin-clone-d0c23.appspot.com",
+//   messagingSenderId: "991154838681",
+//   appId: "1:991154838681:web:9f35ad3049286c8571ac08",
+// };
+
+// const firebaseApp = firebase.initializeApp(firebaseConfig);
+// const db = firebaseApp.firestore();
+// const auth = firebase.auth();
+// const provider = new firebase.auth.GoogleAuthProvider();
+// const storage = firebase.storage();
+
+// export { auth, provider, storage };
+// export default db;
---
+++ src/reducer/index.js
@@ -0,0 +1,8 @@
+// // import React from 'react'
+// import { combineReducers } from 'redux'
+// import userReducer from "./userReducer"
+
+// const rootReducer = combineReducers({
+//     userState:userReducer,
+// })
+// export  default rootReducer
